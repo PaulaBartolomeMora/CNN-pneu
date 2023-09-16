12cb971ed3227063762d310bc2a242289dedd3ac
pbartolome <pbartolome@minsait.com>
2023-09-16 12:56:06 +0200

    preprocess and load

--- functions.py
+++ functions.py
@@ -1,15 +1,26 @@
 
-
-def get_images(dir):
+# 
+def get_images(dir): #dir = chest_xray/train/ o chest_xray/test/
     IMAGES = []
     LABELS = []
     
-    if dir in ['NORMAL']:
-        label = 0
-    elif dir in ['PNEUMONIA']:
-        label = 1
-    else: 
-        label = 2
+    for subdir in os.listdir(dir): #lista de subdirectorios en train/ o test/ -> ['NORMAL', 'PNEUMONIA']
+        if dir in ['NORMAL']:
+            label = 0
+        elif dir in ['PNEUMONIA']:
+            label = 1
+        else: 
+            label = 2
+    
+    for image_name in tqdm(os.listdir(dir+subdir)): #barra de progreso
+        image = cv2.imread(dir + subdir + '/' + image_name) #lectura
+        image_array = preprocess(image)
+        
+        IMAGES.append(image_array)
+        LABELS.append(label)
+        
+    IMAGES = np.asarray(IMAGES)
+    LABELS = np.asarray(LABELS)
     
 
 
